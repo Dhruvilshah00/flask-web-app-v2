ffc33ab4f576cd69121305161fb7fe76c93d0818
Dhruvil Shah <68196930+Dhruvilshah00@users.noreply.github.com>
2023-04-05 10:05:34 +0000

    updated app.py

--- app.py
+++ app.py
@@ -26,12 +26,9 @@ def show_job(id):
 
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
-
   data = request.form
   job = load_job_from_db(id)
-
   add_application_to_db(id, data)
-  
   return render_template('application_submitted.html', application = data, job = job)
 
 
