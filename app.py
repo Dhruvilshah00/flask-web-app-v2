c41f302669b20acd93293318852d97e56b09b3c0
Dhruvil Shah <68196930+Dhruvilshah00@users.noreply.github.com>
2023-04-05 09:54:39 +0000

    store application in database

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_drs():
   jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs, company_name="DRS")
 
+
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
 
 @app.route("/job/<id>")
 def show_job(id):
@@ -20,6 +23,17 @@ def show_job(id):
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
 
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+
+  data = request.form
+  job = load_job_from_db(id)
+
+  add_application_to_db(id, data)
+  
+  return render_template('application_submitted.html', application = data, job = job)
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  
